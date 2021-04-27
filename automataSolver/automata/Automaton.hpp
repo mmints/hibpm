dd3b8ab7029d516ad8d740b4671975674614474b
Jandson <jandson@Jandson-MacBookPro.local>
2021-04-27 16:12:58 +0200

    structure for automata

---
+++ automataSolver/automata/Automaton.hpp
@@ -0,0 +1,39 @@
+//
+//  Automaton.hpp
+//  declareSolver
+//
+//  Created by Jandson on 27.04.21.
+//
+
+#ifndef Automaton_hpp
+#define Automaton_hpp
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <list>
+using namespace std;
+
+class Automaton{
+            
+
+    
+public:
+    
+    vector<vector<int>> *transitionsTo;
+    vector<vector<list<int>>> *incoming;
+    int initialState;
+    vector<bool> areFinalStates;
+    list<int> finalStates;
+     
+    
+    Automaton(int numStates, int sigmaSize);
+    Automaton(int numStates, int sigmaSize, list<int> *finals);
+    
+    void addTransition(int stateFrom, int stateTo, int viaSymbol);
+    void addFinal(int state);
+
+    void print();
+};
+
+#endif /* Automaton_hpp */
