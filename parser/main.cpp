4969eadf9aec7a281f5502093b586e191a887e62
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-07-08 15:21:04 +0200

    [Fix] Add an additional line to the end of the file -> EOF signal is currently missing

--- parser/main.cpp
+++ parser/main.cpp
@@ -20,7 +20,9 @@ std::string readFileIntoString(const std::string& path) {
              << path << "'" << std::endl;
         exit(EXIT_FAILURE);
     }
-    return std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+    std::string str = std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+    str += "\n"; // Add a line to the end of the file.
+    return str;
 }
 
 int main(int argc, char** argv)
