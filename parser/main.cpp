8627533f41af8f88583d2dd2889e09024a9ddb8d
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-07-08 15:04:11 +0200

    [Add] File input

--- parser/main.cpp
+++ parser/main.cpp
@@ -1,6 +1,8 @@
 #include "Declare/Declare.hpp" // Context Header
 #include "declare-parser.hpp" // Parser Header
 
+#include <fstream>
+
 namespace yy
 {
     void declare::error(location const &loc, const std::string& s) {
@@ -8,11 +10,28 @@ namespace yy
     }
 }
 
-int main()
+extern int yy_scan_string(const char *);
+
+
+std::string readFileIntoString(const std::string& path) {
+    std::ifstream input_file(path);
+    if (!input_file.is_open()) {
+        std::cerr << "Could not open the file - '"
+             << path << "'" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    return std::string((std::istreambuf_iterator<char>(input_file)), std::istreambuf_iterator<char>());
+}
+
+int main(int argc, char** argv)
 {
     hibpm::Declare declare_ctx;
-    std::cout << "> ";
     yy::declare parser(declare_ctx); // make a declare parser
+    
+    std::string str = readFileIntoString(argv[1]);
+
+    yy_scan_string(str.c_str());
+ 
     int v = parser.parse(); // and run it
 
     std::cout << "All Events: " << std::endl;
