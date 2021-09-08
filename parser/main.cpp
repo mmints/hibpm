fb9c476f53f0b6329c34e9894e35249c7e816a04
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-09-08 10:41:23 +0200

    update main by adding a default testing path to a declare file

--- parser/main.cpp
+++ parser/main.cpp
@@ -2,14 +2,17 @@
 
 int main(int argc, char** argv)
 {
+    hibpm::Solver parser;
+    hibpm::Declare declareObject;
     if (!argv[1]) {
         std::cout << "Missing Path!" << std::endl;
         std::cout << "Enter a valid path to a declare text file." << std::endl;
-        return 1;
+        std::cout << "Use default path to test file related from the build directory: ../../DECLARE-examples/bpi_2017_kb.txt" << std::endl;
+        declareObject = parser.parseFromFile("../../DECLARE-examples/bpi_2017_kb.txt");
     }
-
-    hibpm::Solver parser;
-    hibpm::Declare declareObject = parser.parseFromFile(argv[1]);
+    else {
+        declareObject = parser.parseFromFile(argv[1]);
+    }
 
     int count = declareObject.getRules().size();
     std::cout << "Count of Rules (Lines): " << std::to_string(count) << std::endl;
