c78a7df7461c79039f39539d7090e2fa3b6c76d4
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-09-15 13:23:48 +0200

    [Add] Testing of the new Proces

--- main.cpp
+++ main.cpp
@@ -1,36 +1,47 @@
 #include "Solver.h"
 
+#include "Process/Process.hpp"
+
 int main(int argc, char** argv)
 {
     hibpm::Solver solver;
-    hibpm::Declare declareObject;
+    hibpm::Declare declareKB;
 
     if (!argv[1]) {
         std::cout << "Missing Path!" << std::endl;
         std::cout << "Enter a valid path to a declare text file." << std::endl;
         std::cout << "Use default path to test file related from the build directory: /Resources/bpi_2017_kb.txt" << std::endl;
-        declareObject = solver.parseFromFile("../Resources/bpi_2017_kb.txt");
+        declareKB = solver.parseFromFile("../Resources/bpi_2017_kb.txt");
     }
     else {
-        declareObject = solver.parseFromFile(argv[1]);
+        declareKB = solver.parseFromFile(argv[1]);
     }
 
+    // Get process and states
+    hibpm::Process process{declareKB};
+    std::vector<hibpm::State> states = process.getStates();
+
     // Testing Print Outs
-    auto count = declareObject.getRules().size();
+    auto count = declareKB.getRules().size();
     std::cout << "Count of Rules (Lines): " << std::to_string(count) << std::endl;
 
     std::cout << "Rules with numeric event values: " << std::endl;
-    for (const auto& rule : declareObject.getRules()) {
+    for (const auto& rule : declareKB.getRules()) {
         std::cout << rule.type <<": Events: " << rule.events.size() << std::endl;
         for (const auto& event : rule.events) {
             std::cout << event.name << " - " << event.numericValue << std::endl;
         }
     }
+    std::cout << "______________________________________" << std::endl;
 
-    std::cout << "All Events (count:" << declareObject.getEvents().size() << "): " << std::endl;
-    for (const auto& e : declareObject.getEvents()) {
+    std::cout << "All Events (count:" << declareKB.getEvents().size() << "): " << std::endl;
+    for (const auto& e : declareKB.getEvents()) {
         std::cout << e.name << "\t Numeric Value: " << e.numericValue << std::endl;
     }
+
+    std::cout << "______________________________________" << std::endl;
+
+    std::cout << "Number of states in Process: " << states.size() << std::endl;
 
     return 0;
 }
