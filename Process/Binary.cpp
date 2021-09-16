0512cdc114cef236166098e03e0657b297e4b728
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-09-16 17:24:57 +0200

    [Add] Binary State implementation

---
+++ Process/Binary.cpp
@@ -0,0 +1,18 @@
+#include "State.hpp"
+
+namespace hibpm
+{
+    Binary::Binary(Rule &rule, size_t sigmaSize) :
+        State(rule, sigmaSize),
+        m_event_1(rule.events[0]),
+        m_event_2(rule.events[1])
+    {}
+
+    std::vector<Event> Binary::getEvents() {
+        return {m_event_1, m_event_2};
+    }
+
+    std::vector<u_int64_t> Binary::getEventNumericValues() {
+        return {m_event_1.numericValue, m_event_2.numericValue};
+    }
+}
