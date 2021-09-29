81380d30cb4584410022e5c23184f6c0c8a23a28
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-09-29 14:55:15 +0200

    [Add] Automaton inizialization

--- Process/Binary.cpp
+++ Process/Binary.cpp
@@ -16,60 +16,143 @@ namespace hibpm
         return {m_event_1.numericValue, m_event_2.numericValue};
     }
 
-    // Constructors
+// ------
     RespondedExistence::RespondedExistence(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         RespondedExistence::initializeAutomaton();
     }
 
+    void RespondedExistence::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     Response::Response(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         Response::initializeAutomaton();
     }
 
+    void Response::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     AlternatedResponse::AlternatedResponse(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         AlternatedResponse::initializeAutomaton();
     }
 
+    void AlternatedResponse::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     ChainResponse::ChainResponse(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         ChainResponse::initializeAutomaton();
     }
 
+    void ChainResponse::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     Precedence::Precedence(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         Precedence::initializeAutomaton();
     }
 
+    void Precedence::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     AlternatedPrecedence::AlternatedPrecedence(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         AlternatedPrecedence::initializeAutomaton();
     }
 
+    void AlternatedPrecedence::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     ChainPrecedence::ChainPrecedence(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         ChainPrecedence::initializeAutomaton();
     }
 
+    void ChainPrecedence::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     CoExistence::CoExistence(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         CoExistence::initializeAutomaton();
     }
 
+    void CoExistence::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     Succession::Succession(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         Succession::initializeAutomaton();
     }
 
+    void Succession::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     AlternateSuccession::AlternateSuccession(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         AlternateSuccession::initializeAutomaton();
     }
 
+    void AlternateSuccession::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     ChainSuccession::ChainSuccession(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         ChainSuccession::initializeAutomaton();
     }
 
+    void ChainSuccession::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     NotChainSuccession::NotChainSuccession(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         NotChainSuccession::initializeAutomaton();
     }
 
+    void NotChainSuccession::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     NotSuccession::NotSuccession(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         NotSuccession::initializeAutomaton();
     }
 
+    void NotSuccession::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
+
+// ------
     NotCoExistence::NotCoExistence(Rule &rule, size_t sigmaSize) : Binary(rule, sigmaSize) {
         NotCoExistence::initializeAutomaton();
     }
+
+    void NotCoExistence::initializeAutomaton() {
+        // TODO: Impl
+    }
+// ------
 }
