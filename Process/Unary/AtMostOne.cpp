f56132f266ff7cbcbed8b5adabb570ca623248d5
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-09-29 15:31:28 +0200

    [Impl] AtMostOne Automaton definition

--- Process/Unary/AtMostOne.cpp
+++ Process/Unary/AtMostOne.cpp
@@ -7,6 +7,21 @@ namespace hibpm
     }
 
     void AtMostOne::initializeAutomaton() {
-        // TODO: Impl
+
+        // (0)---|evLetter|--->(1)
+        m_automaton.addTransition(0, 1, m_event.numericValue);
+
+        // (0)---|Sigma \ evLetter|--->(0)
+        // (1)---|Sigma \ evLetter|--->(1)
+        for (int a = 0; a < m_sigmaSize; a++) {
+            if (a != m_event.numericValue) {
+                m_automaton.addTransition(0, 0, a);
+                m_automaton.addTransition(1, 1, a);
+            }
+        }
+
+        // final
+        m_automaton.addFinal(0);
+        m_automaton.addFinal(1);
     }
 }
