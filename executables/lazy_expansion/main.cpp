fd02144cc23979dad6f198947eee91e1a8d75206
Mark Oliver Mints <mmints@uni-koblenz.de>
2021-12-01 14:42:11 +0100

    [Add] Demo for lazy repair

---
+++ executables/lazy_expansion/main.cpp
@@ -0,0 +1,28 @@
+#include <hibpm/Parser.hpp>
+#include <hibpm/RepairAutomata.hpp>
+
+using namespace hibpm;
+
+int main(int argc, char** argv)
+{
+    Parser parser;
+    parser.parseFromFile("../../../Resources/ruleBase.txt"); // FIXME: Make a relative path in CMake
+    Process process = parser.getProcess();
+
+    cout << process.getStates().at(0)->getRule().toSting() << endl;
+
+    RepairAutomata repairAutomata;
+
+    RemainderComposition remainderComposition;
+    remainderComposition = repairAutomata.lazyExpands(process);
+
+    cout << process.getStates().at(0)->getRule().toSting() << endl;
+    cout << remainderComposition.solutionSet.front()->getRule().toSting() << endl;
+
+    for (auto solution : remainderComposition.solutionSet)
+    {
+        cout << solution->getRule().toSting() << endl;
+    }
+
+    return 0;
+}
