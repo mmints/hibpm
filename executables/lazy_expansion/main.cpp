dd70f69de0bdc6294e83342c7653390109792b67
Mark Oliver Mints <mmints@uni-koblenz.de>
2022-01-31 11:45:52 +0100

    [Update] Print more useful information

--- executables/lazy_expansion/main.cpp
+++ executables/lazy_expansion/main.cpp
@@ -7,7 +7,7 @@ using namespace hibpm;
 int main(int argc, char** argv)
 {
     DeclareParser parser;
-     DeclareContext declareContext = parser.parseFromFile("../../../Resources/ruleBase.txt"); // FIXME: Make a relative path in CMake
+     DeclareContext declareContext = parser.parseFromFile("../../../Resources/chains.txt"); // FIXME: Make a relative path in CMake
 
     DeclareKnowledgeBase declareKnowledgeBase(declareContext);
 
@@ -16,16 +16,32 @@ int main(int argc, char** argv)
     RemainderComposition remainderComposition;
     remainderComposition = repairAutomata.lazyExpands(declareKnowledgeBase);
 
-/*
+    std::cout << "Size of Knowledge Base: " << declareKnowledgeBase.getRuleSet().size() << std::endl;
+    std::cout << "Size of Hitting Set: " << remainderComposition.hittingSet.size() << std::endl;
 
-    for (auto solution : remainderComposition.solutionSet)
+    std::cout << " ---- " << std::endl;
+
+    std::cout << " Hitting Set: " << std::endl;
+
+    for (const auto & hittingSet : remainderComposition.hittingSet)
     {
-        solution->print();
+        hittingSet->print();
     }
 
-*/
-    std::cout << "Size of Knowledge Base: " << declareKnowledgeBase.getRuleSet().size() << std::endl;
-    std::cout << "Size of Solution Set: " << remainderComposition.solutionSet.size() << std::endl;
+    std::cout << " ---- " << std::endl;
+
+    std::cout << " Kernel Set: " << std::endl;
+
+    int i = 0;
+    for (const auto & kernelSet : remainderComposition.kernelSet)
+    {
+        std::cout << " -- " << i << " -- " << std::endl;
+        for (const auto & kernel : kernelSet)
+        {
+            kernel->print();
+        }
+        i++;
+    }
 
     return 0;
 }
