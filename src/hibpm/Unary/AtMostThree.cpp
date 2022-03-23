1e2936e0f29c7d852263466128b0f89b3a4388e8
Mark Oliver Mints <mmints@uni-koblenz.de>
2022-03-23 20:38:54 +0100

    [Impl] Automaton for AtMostThree

--- src/hibpm/Unary/AtMostThree.cpp
+++ src/hibpm/Unary/AtMostThree.cpp
@@ -12,6 +12,32 @@ namespace hibpm
     }
 
     void AtMostThree::initializeAutomaton() {
-        // TODO
+
+        // (0)---|evLetter|--->(2)
+        m_automaton.addTransition(0, 2, m_event.numericValue);
+        // (1)---|evLetter|--->(2)
+        m_automaton.addTransition(1, 2, m_event.numericValue);
+        // (2)---|evLetter|--->(3)
+        m_automaton.addTransition(2, 3, m_event.numericValue);
+        // (3)---|evLetter|--->(4)
+        m_automaton.addTransition(3, 4, m_event.numericValue);
+
+        for (int i = 0; i < m_sigmaSize; i++) {
+            if (i != m_event.numericValue) {
+                // (0)---|Sigma \ evLetter|--->(1)
+                m_automaton.addTransition(0, 1, i);
+                // (1)---|Sigma \ evLetter|--->(1)
+                m_automaton.addTransition(1, 1, i);
+                // (2)---|Sigma \ evLetter|--->(2)
+                m_automaton.addTransition(2, 2, i);
+                // (3)---|Sigma \ evLetter|--->(3)
+                m_automaton.addTransition(3, 3, i);
+                // (4)---|Sigma \ evLetter|--->(4)
+                m_automaton.addTransition(4, 4, i);
+            }
+        }
+
+        // final
+        m_automaton.addFinal(4);
     }
 }
