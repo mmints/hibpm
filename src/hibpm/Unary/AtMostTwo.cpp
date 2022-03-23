d7675ce6fc8b04f0e693f571ae58d31a149b5861
Mark Oliver Mints <mmints@uni-koblenz.de>
2022-03-23 20:41:06 +0100

    [Impl] Automaton for AtMostTwo

--- src/hibpm/Unary/AtMostTwo.cpp
+++ src/hibpm/Unary/AtMostTwo.cpp
@@ -12,6 +12,30 @@ namespace hibpm
     }
 
     void AtMostTwo::initializeAutomaton() {
-        // TODO
+
+        // (0)---|evLetter|--->(2)
+        m_automaton.addTransition(0, 2, m_event.numericValue);
+        // (1)---|evLetter|--->(2)
+        m_automaton.addTransition(1, 2, m_event.numericValue);
+        // (2)---|evLetter|--->(3)
+        m_automaton.addTransition(2, 3, m_event.numericValue);
+
+        for (int i = 0; i < m_sigmaSize; i++) {
+            if (i != m_event.numericValue) {
+                // (0)---|Sigma \ evLetter|--->(1)
+                m_automaton.addTransition(0, 1, i);
+                // (1)---|Sigma \ evLetter|--->(1)
+                m_automaton.addTransition(1, 1, i);
+                // (2)---|Sigma \ evLetter|--->(2)
+                m_automaton.addTransition(2, 2, i);
+                // (3)---|Sigma \ evLetter|--->(3)
+                m_automaton.addTransition(3, 3, i);
+            }
+        }
+
+        // final
+        m_automaton.addFinal(1);
+        m_automaton.addFinal(2);
+        m_automaton.addFinal(3);
     }
 }
