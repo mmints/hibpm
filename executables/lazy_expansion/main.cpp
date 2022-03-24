485287494f2524529a2724a41176e9aa96d4112a
Mark Oliver Mints <mmints@uni-koblenz.de>
2022-03-24 14:16:19 +0100

    [Add] Models with different MINERful settings

--- executables/lazy_expansion/main.cpp
+++ executables/lazy_expansion/main.cpp
@@ -7,23 +7,17 @@ using namespace hibpm;
 int main(int argc, char** argv)
 {
     DeclareParser parser;
-     DeclareContext declareContext = parser.parseFromFile("../../../Resources/ruleBase.txt"); // FIXME: Make a relative path in CMake
-
+    DeclareContext declareContext = parser.parseFromFile("../../../Resources/BPIC2020/models/sub75_conf12-5_int12-5/RequestForPayment.txt"); // FIXME: Make a relative path in CMake
     DeclareKnowledgeBase declareKnowledgeBase(declareContext);
-
     RepairAutomata repairAutomata;
-
     RemainderComposition remainderComposition;
     remainderComposition = repairAutomata.lazyExpands(declareKnowledgeBase);
-
-/*
 
     for (auto solution : remainderComposition.solutionSet)
     {
         solution->print();
     }
 
-*/
     std::cout << "Size of Knowledge Base: " << declareKnowledgeBase.getConstraintSet().size() << std::endl;
     std::cout << "Size of Solution Set: " << remainderComposition.solutionSet.size() << std::endl;
 
